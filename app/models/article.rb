53e7cb751027d5c26b3f90b334164a73b94b417f
chiba <edyzrg1118@gmail.com>
2025-05-13 04:55:48 +0900

    正規表現

--- app/models/article.rb
+++ app/models/article.rb
@@ -12,6 +12,7 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :title, length: { minimum: 2, maximum: 100 }
+  validates :title, format: { with: /\A(?!\@)/ }
 
   # length
   validates :content, presence: true
