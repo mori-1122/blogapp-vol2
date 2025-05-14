9d35a3028628a920c81f18183038a452e028ad21
chiba <edyzrg1118@gmail.com>
2025-05-14 14:41:36 +0900

    add custom validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -19,7 +19,16 @@ class Article < ApplicationRecord
   validates :content, length: { minimum: 10 }
   validates :content, uniqueness: true
 
+  validate :validate_title_and_content_length
+
   def display_created_at
     I18n.l(self.created_at, format: :default)
   end
-end
+
+  private
+  def validate_title_and_content_length
+    char_count = self.title.length + self.content.length
+    errors.add(:content, "100文字以上入力しましょう") unless char_count > 100
+    end
+  end
+
